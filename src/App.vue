2d48e829284274547e7e05c8016b6659b01a8217
mufrick01 <mrmufrick@gmail.com>
2024-10-09 21:20:55 -0300

    formulario bindeado a caja

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+<h1>Formulario sobre la caja</h1>
+<hr>
+<FormularioCaja/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormularioCaja from './components/FormularioCaja.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormularioCaja
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/FormularioCaja.vue
@@ -0,0 +1,117 @@
+<template>
+    <div style="display: flex; gap: 20px; justify-content: center;">
+        <form id="formulario">
+            <label>
+                <h3>bgColor</h3>
+                <input type="color" v-model="bgColor">
+            </label>
+            <label>
+                <h3>textColor</h3>
+                <input type="color" v-model="textColor">
+            </label>
+
+            <label>
+                <h3> border radius</h3>
+                <input v-model="borderRadiusValue" type="range" min="0" max="50">
+            </label>
+
+            <label>
+                <h3>Contenido texto</h3>
+                <textarea v-model="textContent"
+                    placeholder="escribe aquí lo que quieras que aparezca en la caja del lado"></textarea>
+            </label>
+            <label>
+                <h3>Fuente</h3>
+                <select v-model="selectedFont">
+                    <option v-for="( font, index ) in textFonts" :key="`font-${index}`" :value="font">{{ font }}
+                    </option>
+                </select>
+            </label>
+            <label>
+                <h3>Opacity?</h3>
+                <input v-model="opacityCheck" type="checkbox">
+            </label>
+            <label>
+                <h3>tamaño letra</h3>
+                <div style="margin: 0 40px;">
+                    <label for="fs-pequeno" style="font-size:15px">
+                        Pequeño <input v-model="fsize" type="radio" value="15px" name="fsize" id="fs-pequeno">
+                    </label>
+                    <label for="fs-mediano" style="font-size:20px">
+                        Mediano <input v-model="fsize" type="radio" value="25px" name="fsize" id="fs-mediano">
+                    </label>
+                    <label for="fs-grande" style="font-size:30px">
+                        Grande <input v-model="fsize" type="radio" value="40px" name="fsize" id="fs-grande">
+                    </label>
+                </div>
+            </label>
+
+        </form>
+        <article id="caja" :style="{
+            backgroundColor: bgColor,
+            color: textColor,
+            borderRadius: `${borderRadiusValue}%`,
+            fontFamily: selectedFont,
+            opacity: opacityCheck ? 0.5 : 1,
+            fontSize: fsize
+        }">
+            <h2>{{ textContent }}</h2>
+        </article>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data() {
+        return {
+            borderRadiusValue: 0,
+            bgColor: '#333',
+            textColor: '#eee',
+            textContent: 'Awesome Vue',
+            textFonts: ['monospace', 'cursive', 'fantasy'],
+            selectedFont: 'fantasy',
+            opacityCheck: false,
+            fsize: '20px'
+        };
+    },
+};
+</script>
+
+<style scoped>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+#formulario {
+    background-color: #1a0;
+    display: flex;
+    flex-direction: column;
+    color: #eee;
+    width: 700px;
+}
+
+#formulario>label {
+    margin: 0 auto;
+    width: 80%;
+    display: flex;
+    gap: 20px;
+    padding: 10px;
+}
+
+input {
+    flex-grow: 1;
+}
+
+#caja {
+    height: 400px;
+    width: 400px;
+    border: #aaa solid 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
